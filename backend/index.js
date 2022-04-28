f7f1111893e18c449fcc44537a367f419c8f102d
Bettina <BettinaMueller@pm.me>
2022-04-28 16:00:00 +0200

    add bodyparser and cors and dotenv and mongoose and connect to mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
 
 const ports = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send("Hello world!");
-})
+const questionsRoute = require('./routes/questions');
+const usersRoute = require('./routes/users');
+
+//use body-parser middleware for all routes
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use('/questions', questionsRoute);
+app.use('/users', usersRoute);
+
+//connect to db
+console.log(process.env.DB_CONNECTION)
+mongoose.connect(process.env.DB_CONNECTION, () =>
+    console.log('Connected to db!')
+);
 
 app.listen(ports, () => console.log(`Server is running on port ${ports}`));
