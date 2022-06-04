db130e37a163459b22826344d578eb04eed376de
Bettina <BettinaMueller@pm.me>
2022-06-05 00:28:18 +0200

    add the authController within the login and signup method

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,6 +3,10 @@ const User = require('../models/User');
 
 const router = express.Router();
 
+const { body } = require('express-validator');
+
+const authController = require('../controllers/auth');
+
 //get all the users
 router.get('/', async (req, res) => {
     try {
@@ -14,25 +18,25 @@ router.get('/', async (req, res) => {
 });
 
 //add a user to the db
-router.post('/', async (req, res) => {
-    const user = new User({
-        username: req.body.username,
-        password: req.body.password,
-        email: req.body.email
-    })
+router.post(
+    '/signup',
+    [
+        body('username').trim().not().isEmpty(),
+        body('password').trim().isLength({ min: 8}),
+        body('email').isEmail().withMessage('Please enter a valid email.').normalizeEmail()
+    ],
+    authController.signup
+);
 
-    try{
-        const savedUser = await user.save();
-        res.json(savedUser);
-    }catch (err){
-        res.json({message: err});
-    }
-});
+router.post(
+    '/login',
+    authController.login
+);
 
 //get a specific user
-router.get('/:userId', async (req, res) => {
+router.get('/:user_id', async (req, res) => {
     try {
-        const user = await User.findById(req.params.userId);
+        const user = await User.findById(req.params.user_id);
         res.json(user);
     }catch (err){
         res.json({message: err});
@@ -40,9 +44,9 @@ router.get('/:userId', async (req, res) => {
 });
 
 //delete a specific user
-router.delete('/:userId', async (req, res) => {
+router.delete('/:user_id', async (req, res) => {
     try {
-        const removedUser = await User.remove({ _id: req.params.userId });
+        const removedUser = await User.remove({ _id: req.params.user_id });
         res.json(removedUser);
     }catch (err){
         res.json({message: err});
@@ -50,15 +54,13 @@ router.delete('/:userId', async (req, res) => {
 });
 
 //update password of a specific user
-router.patch('/:userId', async (req, res) => {
+router.patch('/:user_id', async (req, res) => {
     try {
-        const updatedUser = await User.updateOne({ _id: req.params.userId }, { $set: {password: req.body.password}});
+        const updatedUser = await User.updateOne({ _id: req.params.user_id }, { $set: {password: req.body.password}});
         res.json(updatedUser);
     }catch (err){
         res.json({message: err});
     }
 });
-
-
 
 module.exports = router;
