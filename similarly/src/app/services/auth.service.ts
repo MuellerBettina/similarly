afb6855cc9f4ae739d93ba04c964b77c9f0c277e
Bettina <BettinaMueller@pm.me>
2022-06-05 00:11:41 +0200

    add signup and login method to the authentication service

--- similarly/src/app/services/auth.service.ts
+++ similarly/src/app/services/auth.service.ts
@@ -1,35 +1,52 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 import { User } from '../models/User';
-import {BehaviorSubject, catchError, first, Observable, tap} from "rxjs";
+import { BehaviorSubject, catchError, first, Observable, tap } from "rxjs";
+import { ErrorHandlerService } from './error-handler.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  private authUrl = 'http://localhost:3000/auth';
+  private authUrl = 'http://localhost:3000/users';
 
   isUserLoggedIn$ = new BehaviorSubject<boolean>(false);
+  userId: Pick<User, 'id'> | undefined
 
   httpOptions: { headers: HttpHeaders } = {
     headers: new HttpHeaders({'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json'} ),
   };
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private errorHandlerService: ErrorHandlerService,
+    private router: Router
+  ) { }
 
-  /*login(email: Pick<User, 'email'>, password: Pick<User, 'password'>): Observable<{
+  signup(user: Omit<User, 'id'>): Observable<User>{
+    return this.http.post<User>(`${this.authUrl}/signup`, user, this.httpOptions).pipe(
+      first(),
+      catchError(this.errorHandlerService.handleError<User>('signup'))
+    );
+  }
+
+  login(email: Pick<User, 'email'>, password: Pick<User, 'password'>): Observable<{
     token: string; userId: Pick<User, 'id'>
   }>{
     return this.http
-      .post(`${this.authUrl}/login`, { email, password }, this.httpOptions)
-      .pipe(first(), tap((tokenObject: { token: string; userId: Pick<User, 'id'>}) => {
-        this.userId = tokenObject.userId;
-        localStorage.setItem('token', tokenObject.token);
-        this.isUserLoggedIn$.next(true);
-      }), catchError(this.errorHandlerService.handleError<{
-        token: string; userId: Pick<User, 'id'>
-      }>('login')));
-  }*/
+      .post<any>(`${this.authUrl}/login`, { email, password }, this.httpOptions)
+      .pipe(
+        first(),
+        tap((tokenObject: { token: string; userId: Pick<User, 'id'> }) => {
+          this.userId = tokenObject.userId;
+          localStorage.setItem('token', tokenObject.token);
+          this.isUserLoggedIn$.next(true);
+        }),
+        catchError(this.errorHandlerService.handleError<{
+          token: string; userId: Pick<User, 'id'>
+        }>('login')));
+  }
 }
