29f074c24a37e177877b4a3172ebb678e0812c52
Bettina <BettinaMueller@pm.me>
2022-06-05 00:13:07 +0200

    add authservice to the registerComponent

--- similarly/src/app/auth/components/register/register.component.ts
+++ similarly/src/app/auth/components/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -9,12 +10,16 @@ import { Router } from '@angular/router';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
   onSubmit(r: NgForm){
+    this.authService.signup(r.value).subscribe((msg) => {
+      console.log(msg);
+      this.router.navigate(['login'])
+    });
     console.log(r.value);
     console.log(r.valid);
   }
