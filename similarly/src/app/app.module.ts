bc01eb4758f3dce619f7786241239c8ce6754b4b
Bettina <BettinaMueller@pm.me>
2022-06-05 00:23:13 +0200

    add new components and material-modules to the app module

--- similarly/src/app/app.module.ts
+++ similarly/src/app/app.module.ts
@@ -2,29 +2,43 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from '@angular/material/icon';
 import { AppComponent } from './app.component';
 import { AuthModule } from "./auth/auth.module";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from "@angular/forms";
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { HttpClientModule } from "@angular/common/http";
+import { NavbarComponent } from './navbar/navbar.component';
+import { PlayComponent } from './play/play.component';
+import { MatCardModule } from "@angular/material/card";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PlayComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
     AuthModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     FormsModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
       // Register the ServiceWorker as soon as the application is stable
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000'
-    })
+    }),
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
