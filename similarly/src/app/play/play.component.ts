f19e02fbe6bf533ccd176c2eda5f876395e7d8d9
Bettina <BettinaMueller@pm.me>
2022-06-05 00:18:38 +0200

    add main play component to the frontend

---
+++ similarly/src/app/play/play.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import {mergeMap, Observable, of, concatAll, Subject, startWith, zip, Subscription, lastValueFrom} from "rxjs";
+import { Question } from "../models/Question";
+import { QuestionService } from "../services/question.service";
+import { AuthService } from "../services/auth.service";
+import { User } from "../models/User";
+import { Selection } from "../models/Selection";
+
+@Component({
+  selector: 'app-play',
+  templateUrl: './play.component.html',
+  styleUrls: ['./play.component.css']
+})
+export class PlayComponent implements OnInit {
+
+  user_id!: Pick<User, "id"> | undefined
+  questions$: Observable<Question[]> | undefined
+  unanswered_questions: Question[] | undefined
+  question$: Observable<Question> | undefined
+
+  questionIndex = 0;
+
+  constructor(
+    private questionService: QuestionService,
+    private authService: AuthService
+  ) { }
+
+  async ngOnInit(): Promise<void> {
+    this.user_id = this.authService.userId
+    this.unanswered_questions = await lastValueFrom(this.getUnansweredQuestions(this.user_id));
+    /*this.questions$ = this.getQuestions();*/
+
+  }
+
+  getUnansweredQuestions(user_id: Pick<User, "id"> | undefined): Observable<Question[]> {
+    console.log(this.questionService.fetchAllUnansweredQuestions(user_id));
+    return this.questionService.fetchAllUnansweredQuestions(user_id);
+  }
+
+  getQuestions(): Observable<Question[]> {
+    return this.questionService.fetchAllQuestions();
+  }
+
+  skipQuestion(): void {
+    if (this.questionIndex !== this.getUnansweredQuestions.length+1){
+      this.questionIndex++;
+    }
+    else {
+      alert("There are no more questions to answer for you.")
+    }
+  }
+
+  saveAnswer(selected_answer: string, question_id: string): void {
+    this.questionService.postSelection(this.user_id, selected_answer, question_id)
+  }
+}
