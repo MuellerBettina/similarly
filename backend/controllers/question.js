ce03f59a599495e27b06da68993f47b9c00055b4
Bettina <BettinaMueller@pm.me>
2022-06-05 13:46:47 +0200

    add question controller with showUnanswered and showAnswered function

---
+++ backend/controllers/question.js
@@ -0,0 +1,105 @@
+const Question = require('../models/Question')
+const Selection = require('../models/Selection')
+
+exports.showUnansweredByUser = async (req, res, next) => {
+
+    const user_id = req.params.user_id;
+
+    const questionIds = await Selection.aggregate(
+        [
+            {
+                $match: {
+                    "user_id": user_id
+                }
+            },
+            {
+                $unset: [
+                    "user_id",
+                    "selected_answer",
+                    "_id",
+                    "__v"
+                ]
+            },
+            {
+                $group: {
+                    "_id": "",
+                    "question_id": {
+                        $push: "$question_id"
+                    }
+                }
+            }
+        ]
+    )
+
+    if(questionIds !== null){
+        const idsArray = questionIds[0].question_id;
+        console.log(idsArray);
+    } else {
+        console.log('this is the else');
+    }
+
+
+        /*if(idsArray !== null){
+            const unansweredQuestions = await Question.aggregate(
+                [
+                    {
+                        $addFields: { _id: { $toString: "$_id" }}
+                    },
+                    {
+                        $match: { _id: { $nin: idsArray}}
+                    }
+                ]
+            )
+            res.json(unansweredQuestions)
+        } else {
+            console.log("all questions are unanswered.")
+            /!*const unansweredQuestions = await Question.aggregate(
+                [
+                    {
+                        $addFields: { _id: { $toString: "$_id" }}
+                    }
+                ]
+            )
+            res.json(unansweredQuestions)*!/
+        }*/
+
+
+}
+
+exports.showAnsweredByUser = async (req, res, next) => {
+
+    const user_id = req.params.user_id;
+
+    try {
+        const questionIds = await Selection.aggregate(
+            [
+                {
+                    $match: {
+                        "user_id": user_id
+                    }
+                },
+                {
+                    $unset: [
+                        "user_id",
+                        "selected_answer",
+                        "_id",
+                        "__v"
+                    ]
+                },
+                {
+                    $group: {
+                        "_id": "",
+                        "question_id": {
+                            $push: "$question_id"
+                        }
+                    }
+                }
+            ]
+        )
+        const idsArray = questionIds[0].question_id
+        const answeredQuestions = await Question.find({"_id": idsArray})
+        res.json(answeredQuestions);
+    } catch (err){
+        res.json({message: "show by user is not working"});
+    }
+}
