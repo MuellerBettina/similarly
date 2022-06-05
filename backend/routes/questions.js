c1eff792e2dc7f4362c06c3fdf134d00955ae266
Bettina <BettinaMueller@pm.me>
2022-06-05 13:48:05 +0200

    add question routes for get and post request

--- backend/routes/questions.js
+++ backend/routes/questions.js
@@ -1,9 +1,46 @@
 const express = require('express');
+const Question = require("../models/Question");
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('We are on questions');
+const questionController = require('../controllers/question')
+
+//get all questions
+router.get('/', async (req, res) => {
+    try {
+        const questions = await Question.find();
+        res.json(questions);
+    } catch (err){
+        res.json({message:err});
+    }
+});
+
+
+router.get(
+    '/unanswered/:user_id',
+    questionController.showUnansweredByUser
+)
+
+router.get(
+    '/answered/:user_id',
+    questionController.showAnsweredByUser
+)
+
+//add one question
+router.post('/', async (req, res) => {
+    const question = new Question({
+        title: req.body.title,
+        body: req.body.body,
+        answer1: req.body.answer1,
+        answer2: req.body.answer2
+    })
+
+    try{
+        const savedQuestion = await question.save();
+        res.json(savedQuestion);
+    }catch (err){
+        res.json({message: err});
+    }
 });
 
 module.exports = router;
